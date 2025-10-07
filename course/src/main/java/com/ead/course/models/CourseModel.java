e3e012729d2cab8e98bc4e79932b179cafa1a2aa
k3n3dfelix <kened.felix@gmail.com>
2025-10-07 09:25:37 -0300

    feat: adicionado mapeamento entre usuarios e cursos com Composition Pattern

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -62,4 +62,8 @@ public class CourseModel implements Serializable {
     //@OnDelete(action = OnDeleteAction.CASCADE) Deleção cascade via banco
     @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private Set<CourseUserModel> courseUsers;
 }
