57102b2f43b0bf02dfb47443a8ce799f98de5271
k3n3dfelix <kened.felix@gmail.com>
2025-10-06 22:09:18 -0300

    feat: Criado relação de ApiComponsition entre users e course

--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -4,11 +4,13 @@ import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -57,5 +59,9 @@ public class UserModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime lastUpdateDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<UserCourseModel> usersCourses;
 
 }
